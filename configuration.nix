e0b4c09bbe9faf8fa55a63f65aa9c4d07e039189
Omar Al Asaad <omarelassaad93@gmail.com>
2022-04-04 00:01:37 +0300

    made some changes

--- configuration.nix
+++ configuration.nix
@@ -129,7 +129,6 @@
    nixpkgs.config.allowUnfree = true;
    environment.systemPackages = with pkgs; [
      #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     nhentai
      wget
      firefox
      google-chrome
